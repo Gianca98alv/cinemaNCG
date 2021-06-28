906789b09f9f4f0935fc482d2f5dfd6469344f0d
Giancarlo Alvarado <giancarlo.alvarado.sanchez@est.una.ac.cr>
2021-06-28 02:40:38 -0600

    Search & Films

--- cinemaNCG/src/main/webapp/js/index.js
+++ cinemaNCG/src/main/webapp/js/index.js
@@ -17,48 +17,49 @@ var login_span = document.getElementsByClassName("login-close")[0]
 var register_span = document.getElementsByClassName("register-close")[0]
 
 // When the user clicks on the button, open the modal
-login_btn.onclick = function() {
-  login_modal.style.display = "block"
+login_btn.onclick = function () {
+    login_modal.style.display = "block"
 }
 
-login_span.onclick = function() {
-  login_modal.style.display = "none"
+login_span.onclick = function () {
+    login_modal.style.display = "none"
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == login_modal) {
-    login_modal.style.display = "none"
-  }
+window.onclick = function (event) {
+    if (event.target == login_modal) {
+        login_modal.style.display = "none"
+    }
 }
 
-register_btn.onclick = function() {
-  register_modal.style.display = "block"
+register_btn.onclick = function () {
+    register_modal.style.display = "block"
 }
 
 // When the user clicks on <span> (x), close the modal
-register_span.onclick = function() {
-  register_modal.style.display = "none"
+register_span.onclick = function () {
+    register_modal.style.display = "none"
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == register_modal) {
-    register_modal.style.display = "none"
-  }
+window.onclick = function (event) {
+    if (event.target == register_modal) {
+        register_modal.style.display = "none"
+    }
 }
 
 var login_submit = document.getElementById("login-submit")
 var idUsuario = document.getElementById("login-cedula")
 var contrasena = document.getElementById("login-contrasena")
-login_submit.onclick = function() {
-    fetch('http://localhost:8080/cinemaNCG/users?idUsuario='+ idUsuario.value +'&contrasena=' + contrasena.value)
-  .then(response => response.json())
-  .then(data => {console.log(data)
-   sessionStorage.setItem('user', data);
-   login_modal.style.display = "none"
-   loadUserOptions();
-   });
+login_submit.onclick = function () {
+    fetch('http://localhost:8080/cinemaNCG/users?idUsuario=' + idUsuario.value + '&contrasena=' + contrasena.value)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                sessionStorage.setItem('user', JSON.stringify(data));
+                login_modal.style.display = "none"
+                loadUserOptions();
+            });
 }
 
 function loadUserOptions() {
@@ -66,15 +67,22 @@ function loadUserOptions() {
         document.getElementById("login-btn").style.display = "none"
         document.getElementById("register-btn").style.display = "none"
         document.getElementById("logout-btn").style.display = "inline"
-        document.getElementById("admin-btn").style.display = "inline"
-        if(!sessionStorage.getItem('user').tipo){
+        let user = sessionStorage.getItem('user')
+        console.log(JSON.parse(sessionStorage.user))
+        if (JSON.parse(sessionStorage.user).tipo != 1) {
             document.getElementById("admin-btn").style.display = "none"
+            document.getElementById("info-btn").style.display = "inline"
+        } else {
+            document.getElementById("info-btn").style.display = "none"
+            document.getElementById("admin-btn").style.display = "inline"
         }
     } else {
         document.getElementById("login-btn").style.display = "inline"
         document.getElementById("register-btn").style.display = "inline"
         document.getElementById("logout-btn").style.display = "none"
         document.getElementById("admin-btn").style.display = "none"
+        document.getElementById("info-btn").style.display = "none"
+
     }
 }
 
@@ -84,7 +92,7 @@ var register_submit = document.getElementById("register-submit")
 var register_idUsuario = document.getElementById("register-cedula")
 var register_nombre = document.getElementById("register-nombre")
 var register_contrasena = document.getElementById("register-contrasena")
-register_submit.onclick = function() {
+register_submit.onclick = function () {
     var url = 'http://localhost:8080/cinemaNCG/users';
     var data = {
         "idUsuario": register_idUsuario.value,
@@ -103,12 +111,46 @@ register_submit.onclick = function() {
             .catch(error => console.error('Error:', error))
             .then(response => {
                 console.log('Success:', response)
-        register_modal.style.display = "none"
-    });
+                register_modal.style.display = "none"
+            });
 }
 
 var logout_btn = document.getElementById("logout-btn")
-logout_btn.onclick = function() {
+logout_btn.onclick = function () {
     sessionStorage.removeItem('user')
     loadUserOptions();
+}
+
+var films_list = document.getElementById("films")
+function loadFilms() {
+    let filmName = document.getElementById("idPelicula").value
+    fetch('http://localhost:8080/cinemaNCG/films?nombrePelicula=' + filmName)
+            .then(response => response.json())
+            .then(data => {
+                let list = "<ul>"
+                let x = 1
+                console.log(data[0].poster)
+                data.forEach(film => {
+                    list += "<li>"
+                    list += "<h3>"+ film.idPelicula +"</h3>"
+                    list += "<img src=\" data:image/jpeg;base64,"+ film.poster +"\" alt=\"Poster\" width=\"150\" height=\"150\">"
+                    list += "</li>"
+                    if (x %4 == 0) {
+                        if (data.length == x) {
+                            list += "</ul>"
+                        } else {
+                            list += "</ul><ul>"
+                        }
+                    }
+                    x++
+                })
+                films_list.innerHTML = list
+            });
+}
+
+loadFilms();
+
+var buscar_btn = document.getElementById("buscar-btn")
+buscar_btn.onclick = function() {
+    loadFilms();
 }
